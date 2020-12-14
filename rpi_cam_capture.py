ec846ee5ff6082e32313a80e79235df0b3d3ae14
Will Chen <will@chendot.com>
2020-12-14 15:16:15 -0800

    Initial commit of Raspberry Pi camera module capture script
    * Initial commit of a 'rpi_cam_capture.py' script that takes a single
      snapshot from attached Raspberry Pi camera module at full resolution

---
+++ rpi_cam_capture.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python
+
+###############################################################################
+# Description:
+#    * Takes a single snapshot from attached camera at full resolution
+#    * Requires attached Raspberry Pi camera module
+#
+# Arguments:
+#    * None
+#
+# Examples:
+#    * ./rpi_cam_capture.py
+#
+# Limitations:
+#    * Tested on only Raspberry Pi 3 Model B
+###############################################################################
+
+# Modules
+import sys
+import time
+from picamera import PiCamera
+
+# Constants
+RES_X = 2592  # Maximum
+RES_Y = 1944  # Maximum
+FRAMERATE = 15
+SLEEP_DURATION_SEC = 5  # Allow time for brightness adaptation
+
+def main(argv):
+    """
+    Main function.
+    """
+
+    # Print current time
+    print(time.strftime("%a %Y-%m-%d %I:%M:%S %p"))
+    print("")
+
+    # Take single snapshot, and then immediately release resources
+    pi_camera = PiCamera()
+    capture(pi_camera)
+    pi_camera.close()
+
+    # Exit
+    print("Done.")
+    print("")
+    sys.exit(0)  # Success
+
+def capture(pi_camera):
+    """
+    Take a snapshot and save it to current directory.
+    """
+
+    # Configuration
+    print("Resolution: {} x {}".format(RES_X, RES_Y))
+    print("Frame rate: {}".format(FRAMERATE))
+    pi_camera.resolution = (RES_X, RES_Y)
+    pi_camera.framerate = FRAMERATE
+
+    # Capture
+    file_name = time.strftime("%Y-%m-%d_%H%M.jpg")
+    print("Capturing 1 still frame and saving it to file '{}'...".format(file_name))
+    pi_camera.start_preview()
+    time.sleep(SLEEP_DURATION_SEC)
+    pi_camera.capture(file_name)
+    pi_camera.stop_preview()
+
+# Execute 'main()' function
+if (__name__ == "__main__"):
+   main(sys.argv)
+
